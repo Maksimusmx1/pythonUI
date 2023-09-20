2b6b962549fa53ccd60821e5b6863e0328448438
Максим Максимов <MAMaksimov.SBT@sberbank.ru>
2023-09-20 11:19:06 +0300

    New

--- test_UI.py
+++ test_UI.py
@@ -4,8 +4,8 @@ import selenium
 import requests
 
 def test_UI():
-    #browser = webdriver.Chrome(r"C:\Program Files (x86)\ChromeDriverSB\sberbrowser_driver_2.0.0.0.exe")
-    browser = selenium.webdriver.Chrome("C:\Program Files (x86)\SberBrowser\Application\sberbrowser.exe").maximize_window()
+    browser = webdriver.Chrome(r"C:\Program Files (x86)\ChromeDriverSB\sberbrowser_driver_2.0.0.0.exe")
+    #browser = selenium.webdriver.Chrome("C:\Program Files (x86)\SberBrowser\Application\sberbrowser.exe").maximize_window()
     # чистим cookies
     browser.delete_all_cookies()
     # ожидаемое время для поиска элемента
