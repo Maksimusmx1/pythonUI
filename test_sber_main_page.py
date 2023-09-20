793a53c49a0892fb9773c5717c75584d77bbf1eb
Максим Максимов <MAMaksimov.SBT@sberbank.ru>
2023-09-20 09:31:51 +0300

    New

--- test_sber_main_page.py
+++ test_sber_main_page.py
@@ -4,11 +4,6 @@ import time
 
 def test_elements_sber_main_page():
     # Initialize Chrome WebDriver
-    #driver = webdriver.Chrome()
-    #driver = webdriver.Chrome("C:\Program Files (x86)\SberBrowser\Application\sberbrowser.exe")
-    #options = webdriver.ChromeOptions()
-    #options.add_argument("--allowed-ips=127.0.0.1")
-    #driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome('C:/Program Files (x86)/ChromeDriverSB/sberbrowser_driver_2.0.0.0.exe')
     # чистим cookies
     driver.delete_all_cookies()
